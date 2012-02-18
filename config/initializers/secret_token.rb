630f079d408654c35475d83f836e0185781f7806
Kazuki Asano <cybaron@gmail.com>
2012-02-18 23:32:36 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catstagram::Application.config.secret_token = '5e97b37b3e00d8b5aea541fced6f305f7d2890cb96e61624fa298179dd4183b2aa1f1fe22a207fed7a59f2a54e37f780c4a512ce8efbd276b5ccccd62f8f89b0'
