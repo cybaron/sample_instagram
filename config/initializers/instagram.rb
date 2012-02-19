713fbb9c467ce4bf646edce8c44a37ffd21cb5e4
Kazuki Asano <cybaron@gmail.com>
2012-02-19 15:41:00 +0900

    instagram.rbの初期化に情報追加

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,7 @@
 require "instagram"
 
 Instagram.configure do |config|
-  config.client_id = "CLIENT_ID"
-  config.client_secret = "CLIENT_SECRET"
+  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
+  config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
+  config.access_token = ENV['INSTAGRAM_ACCESS_TOKEN']
 end
