7417adca1d8f44ff940b6965ba789e1865155b29
Kazuki Asano <cybaron@gmail.com>
2012-02-19 16:37:16 +0900

    development環境ではlib配下のsecret.rbからaccess_tokenを読み込むように修正

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,5 +1,7 @@
 require "instagram"
 
+require 'secret.rb' if Rails.env.development?
+
 Instagram.configure do |config|
   config.client_id = ENV['INSTAGRAM_CLIENT_ID']
   config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
