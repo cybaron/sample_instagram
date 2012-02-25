2f3e2be4ff0f140a32a30201bdc13a3d608336c4
Kazuki Asano <cybaron@gmail.com>
2012-02-25 12:30:19 +0900

    タグの検索機能追加

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,9 +1,17 @@
 class CatsController < ApplicationController
   def index
-    @media_tag = Instagram.tag_recent_media('cat', {
+    if params[:keyword].nil? || params[:keyword] == ""  then
+      keyword = "cat"
+    else
+      keyword = params[:keyword]
+    end
+
+    @media_tag = Instagram.tag_recent_media( keyword, {
       :count => 30
     })
+
     @next_max_id = @media_tag.pagination.next_max_id
     @next_min_id = @media_tag.pagination.next_min_id
+    @keyword = keyword
   end
 end
